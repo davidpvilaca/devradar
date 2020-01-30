b84ec983f9f6b52cd468b7d7c8cbebe01aad7fef
David Vilaça <vilacapdavid@gmail.com>
2020-01-30 10:21:34 -0300

    fix(backend): parse array accept array in parameter

--- backend/src/utils/parseStringAsArray.js
+++ backend/src/utils/parseStringAsArray.js
@@ -1,3 +1,6 @@
 module.exports = function parseStringAsArray (arrayAsString) {
+  if (Array.isArray(arrayAsString)) {
+    return arrayAsString
+  }
   return arrayAsString.split(',').map(tech => tech.trim())
 }
